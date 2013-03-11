478bf894fb14a79074d29d099b6dbc87cc076d85
Marco Leogrande <marcol@plumgrid.com>
2013-03-10 20:07:05 -0700

    Challenge 1

--- 1.py
+++ 1.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python
 
-print 2 ** 32
+import string
+
+str="g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+
+rot2 = string.maketrans(
+ "abcdefghijklmnopqrstuvwxyz",
+ "cdefghijklmnopqrstuvwxyzab")
+
+print string.translate(str, rot2)
+print string.translate('map', rot2)
